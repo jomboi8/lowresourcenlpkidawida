ed87a510312aca9cc12f6fc675daf3f699f348fa
jomboi8 <leonardduncani8@gmail.com>
2026-09-20 11:06:26 +0300

    Delete datadome-bounty directory

--- datadome-bounty/_check_curlcffi_ua.py
+++
@@ -1,6 +0,0 @@
-from curl_cffi.requests import Session
-
-s = Session(impersonate="chrome124")
-r = s.get("https://httpbin.org/headers")
-print(r.status_code)
-print(r.text)
--- datadome-bounty/_check_stealth_api.py
+++
@@ -1,5 +0,0 @@
-import inspect
-from playwright_stealth import Stealth
-
-print(inspect.signature(Stealth.use_sync))
-print(Stealth.use_sync.__doc__)
